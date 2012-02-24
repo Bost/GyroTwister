e47f79a0c803bf16110a360b1160a67a7defbddf
thebost <thebost@gmail.com>
2012-02-24 04:00:52 +0100

    Use map and reify

--- src/clojure/org/bandm/android/SoundActivity.clj
+++ src/clojure/org/bandm/android/SoundActivity.clj
@@ -27,7 +27,7 @@
   (let [
         randNum (java.lang.Math/ceil(* (java.lang.Math/random) 100))
         ]
-      (.setText textField (apply str ["rand: " randNum]))
+      (.setText textField (apply str ["" randNum]))
       ))
 
 (defn -onCreate
@@ -38,31 +38,28 @@
     (.setContentView R$layout/main)
     )
 
-  ; TODO use map for the method findViewById. Example:
-  ; (let [
-  ;        [var0 var1]
-  ;                 (map #(.findViewById this %) [R$id/id0 R$id/id1])
-  ;      ]
-  ; )
   (let
     [
-     valRevolvingSpeed (.findViewById this R$id/valRevolvingSpeed)
-     startRecBtn (.findViewById this R$id/start)
-     stopRecBtn (.findViewById this R$id/stop)
-     clearBtn (.findViewById this R$id/clear)
+;     valRevolvingSpeed (.findViewById this R$id/valRevolvingSpeed)
+;     startRecBtn (.findViewById this R$id/start)
+;     stopRecBtn (.findViewById this R$id/stop)
+;     clearBtn (.findViewById this R$id/clear)
+      [valRevolvingSpeed startRecBtn stopRecBtn clearBtn]
+      (map #(.findViewById this %)
+           [R$id/valRevolvingSpeed R$id/start R$id/stop R$id/clear])
      ]
 
     (.setEnabled stopRecBtn false)
     (.setEnabled startRecBtn true)
 
-    ; create a proxy object clearListener which implements the onClick method
-    ; and attach clearListener to the clearBtn using the setOnClickListener method
-    ; TODO use reify (= 'materialize') instead of proxy because:
+    ; create a proxy object listener which implements the onClick method
+    ; and attach listener to the clearBtn using the setOnClickListener method
+    ; Use reify (= 'materialize') instead of proxy because:
     ; - reify can only implement interfaces. It cannot exted classes (proxy can)
     ; - reify is directly supported by the host platform
     (let [
-          listener (proxy [View$OnClickListener] []
-                     (onClick [view]
+          listener (reify View$OnClickListener
+                     (onClick [this view]
                               (clearHandler valRevolvingSpeed)
                               ))
           ]
@@ -70,8 +67,8 @@
       )
 
     (let [
-          listener (proxy [View$OnClickListener] []
-                     (onClick [view]
+          listener (reify View$OnClickListener
+                     (onClick [this view]
                               (startRecording startRecBtn stopRecBtn)
                               ))
           ]
@@ -79,8 +76,8 @@
       )
 
     (let [
-          listener (proxy [View$OnClickListener] []
-                     (onClick [view]
+          listener (reify View$OnClickListener
+                     (onClick [this view]
                               (stopRecording startRecBtn stopRecBtn)
                               ))
           ]
@@ -89,22 +86,22 @@
 
     (Log/d (str *ns*) "graphview {")
 
+    ;(let [foo (into [] (map #(GraphView$GraphViewData. %1 %2) [1 2] [0.4 0.3]))]
+    ;  (Log/d (str *ns*) foo)
+    ;)
+
     (let
     [
      layout (.findViewById this R$id/layout)
-     arrGraphViewData (into-array [
-                               (GraphView$GraphViewData. 1 0.4)
-                               (GraphView$GraphViewData. 2 0.8)
-                               (GraphView$GraphViewData. 3 1.1)
-                               (GraphView$GraphViewData. 4 1.4)
-                               (GraphView$GraphViewData. 5 1.6)
-                               (GraphView$GraphViewData. 6 1.8)
-                               (GraphView$GraphViewData. 7 1.9)
-                               (GraphView$GraphViewData. 8 2.0)
-                               (GraphView$GraphViewData. 9 2.05)
-                               (GraphView$GraphViewData. 10 2.1)
-                               (GraphView$GraphViewData. 11 2.12)
-                               (GraphView$GraphViewData. 12 2.13)])
+     arrGraphViewData (into-array (map #(GraphView$GraphViewData. %1 %2)
+                      [  1   2   3   4   5   6   7   8    9  10   11   12]
+                      [0.4 0.8 1.1 1.4 1.6 1.8 1.9 2.0 2.05 2.1 2.12 2.13]
+                   ))
+
+;    arrGraphViewData (into-array [(GraphView$GraphViewData. 1 0.4)
+;                                   (GraphView$GraphViewData. 2 0.8)
+;                                   ...
+;                                   (GraphView$GraphViewData. 12 2.13)])
 
      graphView (BarGraphView. this "")    ; this: context; "": heading (the title)
      exampleSeries (GraphView$GraphViewSeries. arrGraphViewData)
